17763335869ed918029edf614862af8c55e02e85
knaus94 <mihitovdima@gmail.com>
2022-08-24 22:53:50 +0300

    .

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const SteamID = require('steamid');
 
 const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
 
-require('util').inherits(SteamGift, require('events').EventEmitter);
-
 module.exports = SteamGift;
 
 /**
