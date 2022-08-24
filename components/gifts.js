4cbc17d5fe69b70d080d7ed0eaf873735d2c59a6
knaus94 <mihitovdima@gmail.com>
2022-08-24 19:17:57 +0300

    .

---
+++ components/gifts.js
@@ -0,0 +1,230 @@
+const Cheerio = require('cheerio');
+const StdLib = require('@doctormckay/stdlib');
+const SteamGift = require('../index.js');
+const SetCookieParser = require('set-cookie-parser');
+
+const EResult = require('../resources/EResult.js');
+SteamGift.prototype.EResult = EResult;
+
+/**
+ * forget Cart
+ * @param {function} [callback]
+ * @returns {Promise}
+ */
+SteamGift.prototype.forgetCart = function (callback) {
+    return StdLib.Promises.callbackPromise(null, callback, true, (accept, reject) => {
+        this.delCookie(`shoppingCartGID`);
+        this.delCookie(`beginCheckoutCart`);
+
+        return accept();
+    });
+};
+
+/**
+ * Add to cart
+ * @param {number} subId - SubId Game to buy
+ * @param {function} [callback]
+ * @returns {Promise}
+ */
+SteamGift.prototype.addToCart = function (subId, callback) {
+    return StdLib.Promises.callbackPromise(null, callback, true, (accept, reject) => {
+        this.request.post({
+            "uri": "https://store.steampowered.com/cart/",
+            "formData": {
+                "sessionid": this.getSessionID(),
+                "action": 'add_to_cart',
+                "snr": '1_5_9__403',
+                "originating_snr": '1_store-navigation__',
+                "subid": subId,
+            }
+        }, (err, response, body) => {
+            if (this._checkHttpError(err, response, reject)) {
+                return;
+            }
+
+            const shoppingCartGID = SetCookieParser.parse(response).find((cookie) => cookie.value === 'shoppingCartGID')
+            if (shoppingCartGID !== undefined) {
+                this.setCookie(`shoppingCartGID=${shoppingCartGID.value}`)
+                this.setCookie(`beginCheckoutCart=${shoppingCartGID.value}`)
+            }
+
+            const $ = Cheerio.load(body);
+
+            const formattedAppId = $('.cart_row.even').attr('data-ds-appid');
+
+            if (formattedAppId === undefined) {
+                return reject(new Error(`Error formatted AppID Id`));
+            }
+
+            return accept({
+                appId: Number(formattedAppId),
+                subId,
+            });
+        });
+    });
+};
+
+/**
+ * checkout Gift Cart
+ * @param {Object[]} items
+ * @param {number} items[].subId
+ * @param {number} items[].appId
+ * @param {function} [callback]
+ * @returns {Promise}
+ */
+SteamGift.prototype.checkoutGiftCart = function (items, callback) {
+    return StdLib.Promises.callbackPromise(null, callback, true, (accept, reject) => {
+        this.request.get("https://store.steampowered.com/cart/", (err, response, body) => {
+            if (this._checkHttpError(err, response, reject)) {
+                return;
+            }
+
+            const $ = Cheerio.load(body);
+
+            /**
+             * @type {number[]}
+             */
+            const cart = [];
+
+            $('.cart_row').each((i, element) => {
+                cart[i] = Number($(element).attr('data-ds-appid'));
+            });
+
+            if (cart.length !== items.length) {
+                return reject(new Error(`Carts don't match`));
+            }
+
+            for (const itm of items) {
+                const i = cart.findIndex((item) => item === itm.appId)
+
+                if (i === -1) {
+                    return reject(new Error(`Not found in cart AppID: ${itm.appId}`));
+                }
+            }
+
+            return accept()
+        });
+    });
+};
+
+/**
+ * init Transaction
+ * @param {string} region Bot Region
+ * @param {number} gifteeAccountID Giftee Account ID
+ * @param {string} gifteeName Giftee Name
+ * @param {string} giftMessage Gift Message
+ * @param {string} giftSentiment Gift Sentiment
+ * @param {string[]} [secondaryLanguages]
+ * @param {function} [callback]
+ * @returns {Promise} 
+ */
+SteamGift.prototype.initTransaction = function (region, gifteeAccountID, gifteeName, giftMessage, giftSentiment, callback) {
+    return StdLib.Promises.callbackPromise(null, callback, true, (accept, reject) => {
+        const cartId = this.getCartId();
+
+        if (cartId === null) {
+            return reject(new Error(`Cookie CartId Not Found`))
+        }
+
+        this.request.post({
+            "uri": "https://store.steampowered.com/checkout/inittransaction/",
+            "formData": {
+                "sessionid": this.getSessionID(),
+                "gidShoppingCart": cartId,
+                "gidReplayOfTransID": -1,
+                "PaymentMethod": 'steamaccount',
+                "abortPendingTransactions": 0,
+                "bHasCardInfo": 0,
+                "Country": region,
+                "bIsGift": 1,
+                "GifteeAccountID": gifteeAccountID,
+                "ScheduledSendOnDate": 0,
+                "bSaveBillingAddress": 1,
+                "bUseRemainingSteamAccount": 1,
+                "bPreAuthOnly": 0,
+                "GifteeName": gifteeName,
+                "GiftMessage": giftMessage,
+                "Sentiment": giftSentiment,
+            },
+            "json": true
+        }, (err, response, body) => {
+            if (this._checkHttpError(err, response, reject)) {
+                return;
+            }
+
+            if (body.success != EResult.OK) {
+                return reject(new Error(EResult[body.success] || "Error " + body.purchaseresultdetail));
+            }
+
+            if (!data.transid) {
+                return reject(new Error('Failed to get steam transid'));
+            }
+
+
+            return accept(data.transid);
+        });
+    });
+};
+
+/**
+ * get Final Price
+ * @param {string} count Count items
+ * @param {string} transid trans id steam
+ * @param {function} [callback]
+ * @returns {Promise}
+ */
+SteamGift.prototype.getFinalPrice = function (count, transid, callback) {
+    return StdLib.Promises.callbackPromise(null, callback, true, (accept, reject) => {
+        const cartId = this.getCartId();
+
+        if (cartId === null) {
+            return reject(new Error(`Cookie CartId Not Found`))
+        }
+
+        this.request.get({
+            "uri": "https://store.steampowered.com/checkout/getfinalprice/",
+            "qs": {
+                "count": count,
+                "transid": transid,
+                "purchasetype": 'gift',
+                "microtxnid": -1,
+                "cart": cartId,
+                "gidReplayOfTransID": -1,
+            }
+        }, (err, response, body) => {
+            if (this._checkHttpError(err, response, reject)) {
+                return;
+            }
+
+            return accept();
+        });
+    });
+};
+
+/**
+ * finalize Transaction
+ * @param {string} transid steam transid
+ * @param {function} [callback]
+ * @returns {Promise} 
+ */
+SteamGift.prototype.finalizeTransaction = function (transid, callback) {
+    return StdLib.Promises.callbackPromise(null, callback, true, (accept, reject) => {
+        this.request.post({
+            "uri": "https://store.steampowered.com/checkout/inittransaction/",
+            "form": {
+                "transid": transid,
+                "CardCVV2": '',
+            }
+        }, (err, response, body) => {
+            if (this._checkHttpError(err, response, reject)) {
+                return;
+            }
+
+            if (data.success === EResult.Pending || data.success === EResult.OK) {
+                return accept();
+            }
+
+            return reject(new Error(EResult[body.success] || "Error " + body.purchaseresultdetail));
+        });
+    });
+};
---
+++ index.js
@@ -0,0 +1,129 @@
+const Request = require('request');
+const SteamID = require('steamid');
+
+const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
+
+module.exports = SteamGift;
+
+/**
+ * Create a new SteamGift instance.
+ * @param {object} [options]
+ * @constructor
+ */
+ function SteamGift(options) {
+	options = options || {};
+
+	this._jar = Request.jar();
+
+	let defaults = {
+		"jar": this._jar,
+		"timeout": options.timeout || 50000,
+		"gzip": true,
+		"headers": {
+			"User-Agent": options.userAgent || USER_AGENT
+		}
+	};
+
+	this.request = options.request || Request.defaults({"forever": true});  // "forever" indicates that we want a keep-alive agent
+	this.request = this.request.defaults(defaults);
+
+	// UTC, English
+	this.setCookie("timezoneOffset=0,0");
+	this.setCookie("Steam_Language=english");
+}
+
+/**
+ * Get CartId
+ * @returns {string|null}
+ */
+ SteamGift.prototype.getCartId = function() {
+	let cookies = this._jar.getCookieString("http://store.steampowered.com").split(';');
+	for (let i = 0; i < cookies.length; i++) {
+		let match = cookies[i].trim().match(/([^=]+)=(.+)/);
+		if (match[1] == 'shoppingCartGID') {
+			return match[2];
+		}
+	}
+
+	return null;
+};
+
+/**
+ * Set a single cookie on this SteamGift instance.
+ * @param {string} cookie - In format "cookieName=cookieValue"
+ */
+ SteamGift.prototype.setCookie = function(cookie) {
+	let cookieName = cookie.match(/(.+)=/)[1];
+	if (cookieName == 'steamLogin' || cookieName == 'steamLoginSecure') {
+		this.steamID = new SteamID(cookie.match(/=(\d+)/)[1]);
+	}
+
+	let isSecure = !!cookieName.match(/(^steamMachineAuth|^steamLoginSecure$)/);
+	this._jar.setCookie(Request.cookie(cookie), (isSecure ? "https://" : "http://") + "store.steampowered.com");
+	this._jar.setCookie(Request.cookie(cookie), (isSecure ? "https://" : "http://") + "steamcommunity.com");
+};
+
+/**
+ * Delete a single cookie on this SteamGift instance.
+ * @param {string} cookieName - Cookie Name
+ */
+ SteamGift.prototype.delCookie = function(cookieName) {
+	let cookie = cookieName + "=null";
+
+	let isSecure = !!cookieName.match(/(^steamMachineAuth|^steamLoginSecure$)/);
+	this._jar.setCookie(Request.cookie(cookie), (isSecure ? "https://" : "http://") + "store.steampowered.com", new Date(+0));
+	this._jar.setCookie(Request.cookie(cookie), (isSecure ? "https://" : "http://") + "steamcommunity.com", new Date(+0));
+};
+
+function generateSessionID() {
+	return Math.floor(Math.random() * 1000000000);
+}
+
+/**
+ * Set multiple cookies.
+ * @param {string[]} cookies
+ */
+ SteamGift.prototype.setCookies = function(cookies) {
+	cookies.forEach(this.setCookie.bind(this));
+};
+
+/**
+ * Get this SteamGift instance's sessionid CSRF token.
+ * @returns {string}
+ */
+ SteamGift.prototype.getSessionID = function() {
+	let cookies = this._jar.getCookieString("http://store.steampowered.com").split(';');
+	for (let i = 0; i < cookies.length; i++) {
+		let match = cookies[i].trim().match(/([^=]+)=(.+)/);
+		if (match[1] == 'sessionid') {
+			return decodeURIComponent(match[2]);
+		}
+	}
+
+	let sessionID = generateSessionID();
+	this.setCookie("sessionid=" + sessionID);
+	return sessionID;
+};
+
+SteamGift.prototype._checkHttpError = function(err, response, callback) {
+	if (err) {
+		callback(err);
+		return true;
+	}
+
+	if (response.statusCode >= 300 && response.statusCode <= 399 && response.headers.location.indexOf('/login') != -1) {
+		callback(new Error("Not Logged In"));
+		return true;
+	}
+
+	if (response.statusCode >= 400) {
+		let error = new Error("HTTP error " + response.statusCode);
+		error.code = response.statusCode;
+		callback(error);
+		return true;
+	}
+
+	return false;
+};
+
+require('./components/gifts.js');
