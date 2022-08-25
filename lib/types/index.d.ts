d62edc1f8bd6c165ffdb3c295a2f530fc686296f
knaus94 <mihitovdima@gmail.com>
2022-08-25 14:24:53 +0300

    .

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -31,14 +31,14 @@ declare class SteamGift {
   /**
    * Transaction initialization. Returns the Steam transaction ID if successful
    * @param region Bot Region
-   * @param gifteeAccountID Giftee Account ID
+   * @param gifteeSteamID Giftee SteamID
    * @param gifteeName Giftee Name
    * @param giftMessage Gift Message
    * @param giftSentiment Gift Sentiment
    */
   initTransaction(
     region: string,
-    gifteeAccountID: number,
+    gifteeSteamID: string,
     gifteeName: string,
     giftMessage: string,
     giftSentiment: string
