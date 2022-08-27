7bb8d508af899b1cd4e887af11c12a854ea41745
knaus94 <mihitovdima@gmail.com>
2022-08-27 15:48:46 +0300

    .

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,4 +1,5 @@
 import SteamID from "steamid";
+const TransResult = require('../resources/TransResult.js');
 
 export = SteamGift;
 
@@ -69,7 +70,13 @@ declare class SteamGift {
    * @param transid Steam Transaction ID
    * @param count Items count in Transaction
    */
-  getTransactionStatus(transid: string, count: number): Promise<number>;
+  getTransactionStatus(transid: string, count: number): Promise<TransResult>;
+}
+
+export enum TransResult {
+  "Invalid" = 0,
+  "Declined" = 1,
+	"None" = 2
 }
 
 interface Options {
